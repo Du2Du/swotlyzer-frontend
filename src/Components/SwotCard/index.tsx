f5a869ce6a58d141655b8151421357293c2291e3
andr30z <andrelp1015@gmail.com>
2022-10-07 21:35:24 -0300

    style: change dashboard styles

--- src/Components/SwotCard/index.tsx
+++ src/Components/SwotCard/index.tsx
@@ -1,49 +1,41 @@
-import {
-  Badge,
-  Box,
-  Flex,
-  Image,
-  Portal,
-  useSafeLayoutEffect,
-} from "@chakra-ui/react";
-import html2canvas from "html2canvas";
-import React, { useEffect, useRef, useState } from "react";
+import { Badge, Box } from "@chakra-ui/react";
+import React from "react";
 import { SwotAnalysis, SwotLayoutDictionary } from "../../GlobalInterface/Swot";
-import { SwotFiedList } from "../SwotFieldList";
 
 export const SwotCard: React.FC<SwotAnalysis & { index: number }> = (props) => {
   //   const printContainer = useRef<HTMLDivElement>(null);
   const { index, ...rest } = props;
+  const colors = ["red.400", "white.400", "cyan.400"]
   const { swotLayoutType, swotTemplate, title } = rest;
-  const [previewURL, setPreviewURL] = useState<string>();
-  const [isLoading, setIsLoading] = useState(false);
-  const [hasFailedPreview, setHasFailedPreview] = useState(false);
+  // const [previewURL, setPreviewURL] = useState<string>();
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [hasFailedPreview, setHasFailedPreview] = useState(false);
 
-  const handleDownloadImage = async (element: HTMLDivElement) => {
-    console.log(element);
-    console.log(isLoading, previewURL, hasFailedPreview);
-    if (isLoading || previewURL || hasFailedPreview) return;
-    console.log("PASSEI");
-    try {
-      setIsLoading(true);
-      const canvas = await html2canvas(element as HTMLElement, {
-        height: 360,
-        width: 400,
-      });
+  // const handleDownloadImage = async (element: HTMLDivElement) => {
+  //   console.log(element);
+  //   console.log(isLoading, previewURL, hasFailedPreview);
+  //   if (isLoading || previewURL || hasFailedPreview) return;
+  //   console.log("PASSEI");
+  //   try {
+  //     setIsLoading(true);
+  //     const canvas = await html2canvas(element as HTMLElement, {
+  //       height: 360,
+  //       width: 400,
+  //     });
 
-      const data = canvas.toDataURL("image/png");
-      setPreviewURL(data);
-    } catch (error) {
-      console.warn(error);
-      setIsLoading(false);
-      setHasFailedPreview(true);
-    }
-  };
+  //     const data = canvas.toDataURL("image/png");
+  //     setPreviewURL(data);
+  //   } catch (error) {
+  //     console.warn(error);
+  //     setIsLoading(false);
+  //     setHasFailedPreview(true);
+  //   }
+  // };
 
   return (
     <Box
-      w="330px"
-      h="350px"
+      w="100%"
+      h="100%"
       borderRadius="lg"
       bg="white"
       overflow="hidden"
@@ -55,15 +47,7 @@ export const SwotCard: React.FC<SwotAnalysis & { index: number }> = (props) => {
         transform: "scale(1.02)",
       }}
     >
-      <Box h="60%">
-        <Image
-          boxSize="100%"
-          objectFit="contain"
-          filter="blur(4px)"
-          src={previewURL}
-          alt={title}
-        />
-      </Box>
+      <Box bg={colors[index]} h="60%"/>
       <Box bg="blackAlpha.700" h="40%" p="5">
         <Box display="flex" alignItems="baseline">
           <Box letterSpacing="wide" fontSize="xs">
@@ -90,19 +74,6 @@ export const SwotCard: React.FC<SwotAnalysis & { index: number }> = (props) => {
           {title}
         </Box>
       </Box>
-      <Portal appendToParentPortal={false}>
-        {/* HACK */}
-        <Box position="relative" overflow="hidden">
-          <Box
-            position="absolute"
-            ref={handleDownloadImage}
-            top={19999999 * (index + 1)}
-            right={300000000}
-          >
-            <SwotFiedList {...rest} />
-          </Box>
-        </Box>
-      </Portal>
     </Box>
   );
 };
--- src/PageModules/Dashboard/AsideUserInfo.tsx
+++ src/PageModules/Dashboard/AsideUserInfo.tsx
@@ -1,34 +1,35 @@
-import { Avatar, Box, Center, Heading, Link } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Center,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerOverlay,
+  Heading,
+  Link,
+  useDisclosure,
+} from "@chakra-ui/react";
 import NextLink from "next/link";
 import React from "react";
 import { RoutesName } from "../../RoutesName";
 import { Button } from "../../Components/Button";
+import { HamburgerIcon } from "@chakra-ui/icons";
 
 interface AsideUserInfoProps {
   isOpen: boolean;
 }
 
 export const AsideUserInfo: React.FC<AsideUserInfoProps> = ({}) => {
-  return (
-    <Box
-      as="aside"
-      bg="whiteAlpha.900"
-      height="100vh"
-      position="sticky"
-      left="0"
-      top="0"
-      display="flex"
-      roundedRight="3xl"
-      alignItems="center"
-      justifyContent="space-between"
-      flexDirection="column"
-      width="10%"
-      padding={4}
-      
-    >
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const content = (
+    <>
       <Center flexDir="column">
-        <Heading size="lg">SWOTLYZER</Heading>
-        <Center flexDir="column">
+        <Heading flex={1} size="lg">
+          SWOTLYZER
+        </Heading>
+        <Center flex={1} flexDir="column">
           <Avatar
             loading="eager"
             size="2xl"
@@ -41,7 +42,7 @@ export const AsideUserInfo: React.FC<AsideUserInfoProps> = ({}) => {
         </Center>
       </Center>
 
-      <Center flexDirection="column" w="100%">
+      <Center flex={1} flexDirection="column" w="100%">
         <NextLink href={RoutesName.PROFILE} passHref>
           <Button size="sm" w="100%" color="white" bg="blackAlpha.800" as="a">
             Profile
@@ -53,13 +54,67 @@ export const AsideUserInfo: React.FC<AsideUserInfoProps> = ({}) => {
           </Button>
         </NextLink>
       </Center>
-      <Center w="100%" alignSelf="flex-end">
+      <Center flex={1} w="100%">
         <NextLink href={RoutesName.LOGOUT} passHref>
-          <Button size="sm" w="100%" color="white" bg="red.400" as="a">
+          <Button
+            alignSelf="flex-end"
+            size="sm"
+            w="100%"
+            color="white"
+            bg="red.400"
+            as="a"
+          >
             Logout
           </Button>
         </NextLink>
       </Center>
-    </Box>
+    </>
+  );
+  return (
+    <>
+      <Button
+        zIndex={100}
+        position="fixed"
+        top={15}
+        left={15}
+        display={["unset", "unset", "unset", "none"]}
+        colorScheme="white"
+        onClick={onOpen}
+        borderColor="white"
+        shadow="lg"
+        opacity={0.5}
+        _hover={{
+          opacity: 1,
+        }}
+      >
+        <HamburgerIcon color="red" boxSize={6} />
+      </Button>
+      <Box
+        as="aside"
+        bg="whiteAlpha.900"
+        height="100vh"
+        position={["unset", "unset", "unset", "sticky"]}
+        left="0"
+        top="0"
+        display={["none", "none", "none", "flex"]}
+        roundedRight="3xl"
+        alignItems="center"
+        justifyContent="space-between"
+        flexDirection="column"
+        width={["20%","17%"]}
+        padding={4}
+      >
+        {content}
+      </Box>
+      <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerBody height="100%" display="flex" flexDirection="column">
+            {content}
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
   );
 };
