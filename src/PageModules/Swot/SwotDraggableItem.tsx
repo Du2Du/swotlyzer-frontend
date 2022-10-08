67844b44e32795994c8f6ff17d224386771b1d5f
andr30z <andrelp1015@gmail.com>
2022-10-08 15:24:47 -0300

    feature: create swot modules

---
+++ src/PageModules/Swot/SwotDraggableItem.tsx
@@ -0,0 +1,17 @@
+import { Text } from "@chakra-ui/react";
+import React from "react";
+import { SwotField } from "../../GlobalInterface/Swot";
+
+interface SwotDraggableItemProps {
+  item: SwotField;
+}
+
+/**
+ *
+ * @author andr30z
+ **/
+export const SwotDraggableItem: React.FC<SwotDraggableItemProps> = ({
+  item: { text, _id, ...rest },
+}) => {
+  return <Text {...rest}>{text}</Text>;
+};
---
+++ src/PageModules/Swot/SwotDroppableArea.tsx
@@ -0,0 +1,56 @@
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+import { Draggable, DraggableChildrenFn, Droppable } from "react-beautiful-dnd";
+import { SwotArea } from "../../GlobalInterface/Swot";
+import { SwotDraggableItem } from "./SwotDraggableItem";
+
+interface SwotDroppableAreaProps {
+  droppableId: string;
+  item: SwotArea;
+}
+
+export const SwotDroppableArea: React.FC<SwotDroppableAreaProps> = ({
+  droppableId,
+  item: { fields, bgColor },
+}) => {
+  const renderDroppableClone: DraggableChildrenFn = (
+    provided,
+    _snapshot,
+    rubric
+  ) => {
+    const item = fields[rubric.source.index];
+    return (
+      <div
+        {...provided.draggableProps}
+        {...provided.dragHandleProps}
+        ref={provided.innerRef}
+        id={item._id}
+        //meowwww
+      >
+        <SwotDraggableItem item={item} />
+      </div>
+    );
+  };
+  return (
+    <Droppable droppableId={droppableId} renderClone={renderDroppableClone}>
+      {(provided, _snapshot) => (
+        <Flex
+          bgColor={bgColor}
+          ref={provided.innerRef}
+          {...provided.droppableProps}
+          flexDirection="column"
+          p={3}
+          h="100%"
+          w="100%"
+        >
+          {fields.map((item, index) => (
+            <Draggable key={item._id} draggableId={item._id} index={index}>
+              {renderDroppableClone}
+            </Draggable>
+          ))}
+          {provided.placeholder}
+        </Flex>
+      )}
+    </Droppable>
+  );
+};
