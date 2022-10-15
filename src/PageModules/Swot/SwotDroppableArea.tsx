95553d5f3c3365a042737354886b2d0e6569a586
andr30z <andrelp1015@gmail.com>
2022-10-14 21:38:51 -0300

    fix: remove memo to avoid wrong behaviour

--- src/PageModules/Swot/SwotDroppableArea.tsx
+++ src/PageModules/Swot/SwotDroppableArea.tsx
@@ -9,47 +9,48 @@ interface SwotDroppableAreaProps {
   item: SwotArea;
 }
 
-export const SwotDroppableArea = React.memo<SwotDroppableAreaProps>(
-  ({ droppableId, item: { fields, bgColor } }) => {
-    const renderDroppableClone: DraggableChildrenFn = (
-      provided,
-      _snapshot,
-      rubric
-    ) => {
-      const item = fields[rubric.source.index];
-      return (
-        <div
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-          ref={provided.innerRef}
-          id={item._id}
-          //meowwww
-        >
-          <SwotDraggableItem item={item} />
-        </div>
-      );
-    };
+export const SwotDroppableArea: React.FC<SwotDroppableAreaProps> = ({
+  droppableId,
+  item: { fields, bgColor },
+}) => {
+  const renderDroppableClone: DraggableChildrenFn = (
+    provided,
+    _snapshot,
+    rubric
+  ) => {
+    const item = fields[rubric.source.index];
     return (
-      <Droppable droppableId={droppableId} renderClone={renderDroppableClone}>
-        {(provided, _snapshot) => (
-          <Flex
-            bgColor={bgColor}
-            ref={provided.innerRef}
-            {...provided.droppableProps}
-            flexDirection="column"
-            p={3}
-            h="100%"
-            w="100%"
-          >
-            {fields.map((item, index) => (
-              <Draggable key={item._id} draggableId={item._id} index={index}>
-                {renderDroppableClone}
-              </Draggable>
-            ))}
-            {provided.placeholder}
-          </Flex>
-        )}
-      </Droppable>
+      <div
+        {...provided.draggableProps}
+        {...provided.dragHandleProps}
+        ref={provided.innerRef}
+        id={item._id}
+        //meowwww
+      >
+        <SwotDraggableItem item={item} />
+      </div>
     );
-  }
-);
+  };
+  return (
+    <Droppable droppableId={droppableId} renderClone={renderDroppableClone}>
+      {(provided, _snapshot) => (
+        <Flex
+          bgColor={bgColor}
+          ref={provided.innerRef}
+          {...provided.droppableProps}
+          flexDirection="column"
+          p={3}
+          h="100%"
+          w="100%"
+        >
+          {fields.map((item, index) => (
+            <Draggable key={item._id} draggableId={item._id} index={index}>
+              {renderDroppableClone}
+            </Draggable>
+          ))}
+          {provided.placeholder}
+        </Flex>
+      )}
+    </Droppable>
+  );
+};
